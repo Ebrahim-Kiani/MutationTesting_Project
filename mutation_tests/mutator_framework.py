a5f42c53ab39db319ab1a91621722b5f41c24c0b
mohammadmazrouei <mohammademazrouei@gmail.com>
2025-01-05 12:04:14 +0330

    Add Class CODMutator

--- mutation_tests/mutator_framework.py
+++ mutation_tests/mutator_framework.py
@@ -54,6 +54,7 @@ class BCRMutator():
         return finder.statements
 
     def generate_mutated_codes(self):
+        self.mutated_codes = []
         statements = self.find_break_continue_statements()
         n = min(self.n, len(statements))
 
@@ -67,6 +68,67 @@ class BCRMutator():
 
             # Mutate the target statement
             mutator = self.SingleBCRMutator(target_index=i)
+            mutator.visit(mutated_tree)
+
+            # Fix the tree and convert back to code
+            ast.fix_missing_locations(mutated_tree)
+            mutated_code = astor.to_source(mutated_tree)
+            self.mutated_codes.append(mutated_code)
+
+
+class CODMutator():
+    """
+    A class to perform Conditional Operator Deletion (COD) mutation on a given AST.
+    """
+    def __init__(self, tree, n):
+        self.tree = tree
+        self.n = n
+        self.mutated_codes = []
+
+    class SingleCODMutator(ast.NodeTransformer):
+        """
+        A class to perform single Conditional Operator Deletion (COD) mutation.
+        """
+        def __init__(self, target_index):
+            super().__init__()
+            self.target_index = target_index
+            self.current_index = -1
+
+        def visit_Compare(self, node):
+            self.current_index += 1
+            if self.current_index == self.target_index:
+                return ast.Constant(value=random.choice([True, False]))  # Replace with a valid constant
+            return node
+
+    def find_conditional_operators(self):
+        class Finder(ast.NodeVisitor):
+            def __init__(self):
+                self.operators = []
+
+            def visit_Compare(self, node):
+                self.operators.append(('compare', node.lineno))
+                self.generic_visit(node)
+
+        finder = Finder()
+        finder.visit(self.tree)
+        return finder.operators
+
+    def generate_mutated_codes(self):
+        self.mutated_codes = []
+        operators = self.find_conditional_operators()
+        n = min(self.n, len(operators))
+        print(n)
+
+        random_indices = list(range(len(operators)))
+        random.shuffle(random_indices)
+
+        # Generate mutated versions
+        for i in random_indices[:n]:
+            # Deep copy the original tree
+            mutated_tree = deepcopy(self.tree)
+
+            # Mutate the target conditional operator
+            mutator = self.SingleCODMutator(target_index=i)
             mutator.visit(mutated_tree)
 
             # Fix the tree and convert back to code
